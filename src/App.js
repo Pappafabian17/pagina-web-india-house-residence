afd6ea0d93c78d0bec33cbc9acf5e016ba5abc28
Fabian Pappa <fabian.pappa.ext@errepar.com>
2023-12-20 21:38:40 -0300

    estilos cards

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ function App() {
                 <Cuerpo />
                 <About />
                 <SoundcloudPlayer />
-                <Card />
               </>
             }
           />
